144a4946c3c204dfe2294e5e695650a46ac5f57e
Rui Yang <reyyang00@gmail.com>
2021-02-28 15:37:25 -0800

    lab03

--- hw01/lab03/guess_key.py
+++ hw01/lab03/guess_key.py
@@ -1,5 +1,19 @@
-end = 1524006529
-start = end - 7200
+from binascii import hexlify, unhexlify
+from Crypto.Cipher import AES
 
-for i in range(end, start, -1):
-	print(i)
+iv = '09080706050403020100A2B2C2D2E2F2'
+p = '255044462d312e350a25d0d4c5d80a34'
+c = 'd06bf9d0dab8e8ef880660d2af65aa82'
+
+file1=open('keys.txt', 'r')
+lineList = [line.rstrip('\n') for line in file1]
+file1.close()
+
+#for s in lineList:
+#	print(s)
+
+
+cipher =AES.new(lineList[1], AES.MODE_CBC, iv);
+
+print(cipher)
+
--- hw01/lab03/random-number-generator.c
+++ hw01/lab03/random-number-generator.c
@@ -1,20 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+
 #define KEYSIZE 16
 
 
-void main()
+void key_gen(int t);
+
+int main()
 {
-	int i;
-	char key[KEYSIZE];
-	printf("%lld\n", (long long) time(NULL));
+	for(int i = 1524006529; i >= (1524006529-7200);i--){
+		key_gen(i);
+	}
 	
-	//srand (time(NULL));
-	srand (1614197798); 
-	for (i = 0; i< KEYSIZE; i++){
-		key[i] = rand()%256;
-     	printf("%.2x", (unsigned char)key[i]);
-  	}
-  	printf("\n");
+	return 0;
+}
+
+void key_gen(int t) {
+	int i;
+    char key[KEYSIZE];
+
+    //srand (time(NULL));
+    srand (t);
+	FILE *file = fopen("keys.txt", "a");
+    for (i = 0; i< KEYSIZE; i++){
+        key[i] = rand()%256;
+        printf("%.2x", (unsigned char)key[i]);
+		fprintf(file, "%.2x", (unsigned char)key[i]);
+    }
+
+	fprintf(file, "\n");
+	fclose(file);
+
+	printf("\n");
 }
